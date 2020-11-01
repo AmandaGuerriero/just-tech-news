224e384a2d23345532095dc6db2c6c2b89e4e156
Amanda Guerriero <ag.codingclass@gmail.com>
2020-11-01 12:38:00 -0800

    Updated with given code

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-DROP DATABASE IF EXISTS just_tech_news_db;
+DROP DATABASE IF EXISTS  just_tech_news_db;
 
-CREATE DATABASE just_tech_news_db;
-
-CREATE TABLE user;
+CREATE DATABASE  just_tech_news_db;
